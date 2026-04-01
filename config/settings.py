a0b957e61d29a382a97a3110ea83c1c67466049d
user <maxalone155@gmail.com>
2026-04-01 15:09:32 +0500

    sirojiddin

--- config/settings.py
+++ config/settings.py
@@ -243,10 +243,7 @@ MODERN_DRF_SWAGGER = {
 }
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = config(
-    'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://localhost:8000'
-).split(',')
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT',
