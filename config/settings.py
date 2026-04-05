b4ffe939adeb8e6e77216801f8bb202f2b76faa7
Muxriddin <muxriddin@example.com>
2026-04-05 18:17:34 +0500

    'a'

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
